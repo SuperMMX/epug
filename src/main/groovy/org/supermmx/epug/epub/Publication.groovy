0fb0fba7054195dfbf543313c9905a79e1fb5743
Xuan MA <SuperMMX@gmail.com>
2015-11-27 21:15:37 +1100

    Add publication

---
+++ src/main/groovy/org/supermmx/epug/epub/Publication.groovy
@@ -0,0 +1,28 @@
+package org.supermmx.epug.epub
+
+import org.supermmx.epug.storage.Storage
+import org.supermmx.epug.storage.MemoryStorage
+
+/**
+ * An Epub publication that contains one or more renditions,
+ * and will be packaged in the container .epub file.
+ *
+ * It contains only one corresponding container.xml
+ */
+class Publication {
+    List<Rendition> renditions = [ new Rendition() ]
+
+    Rendition getDefaultRendition() {
+        def rendition = null
+
+        if (renditions.size() > 0) {
+            rendition = renditions[0]
+        }
+
+        return rendition
+    }
+
+    Rendition getRendition() {
+        return getDefaultRendition()
+    }
+}
