f38342e5f0667bf4ef2c9f095ecc20f352713595
Xuan MA <SuperMMX@gmail.com>
2016-01-06 22:14:17 +1100

    Extend DcmiTerm from DcTerm

--- src/main/groovy/org/supermmx/epug/epub/dcmi/DcmiTerm.groovy
+++ src/main/groovy/org/supermmx/epug/epub/dcmi/DcmiTerm.groovy
@@ -1,6 +1,6 @@
 package org.supermmx.epug.epub.dcmi
 
-class DcmiTerm {
+class DcmiTerm extends DcTerm {
     static final String BASE_URI = 'http://purl.org/dc/terms/'
     static final DcmiTerm modified = [baseUri: BASE_URI, name: 'modified'] as DcmiTerm
 }
