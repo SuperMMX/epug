2ced7d48e84fa5ddbe4627ec02925177b0b8ee18
Xuan MA <SuperMMX@gmail.com>
2016-01-06 20:28:56 +1100

    Add identifier dc term

--- src/main/groovy/org/supermmx/epug/epub/dcmi/DcesTerm.groovy
+++ src/main/groovy/org/supermmx/epug/epub/dcmi/DcesTerm.groovy
@@ -4,6 +4,7 @@ class DcesTerm extends DcTerm {
     static final String BASE_URI = 'http://purl.org/dc/elements/1.1/'
 
     static final DcesTerm contributer = [ baseUri: BASE_URI, name:'contributer'] as DcesTerm
+    static final DcesTerm identifier = [ baseUri: BASE_URI, name:'identifier'] as DcesTerm
     static final DcesTerm title = [ baseUri: BASE_URI, name:'title'] as DcesTerm
     static final DcesTerm language = [ baseUri: BASE_URI, name:'language'] as DcesTerm
 }
