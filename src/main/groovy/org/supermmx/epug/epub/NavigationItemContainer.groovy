1e3c4ed024cda003ffc481acef9a593a6ddc9a13
Xuan MA <SuperMMX@gmail.com>
2016-03-21 22:31:32 +1100

    Add left shift operator

--- src/main/groovy/org/supermmx/epug/epub/NavigationItemContainer.groovy
+++ src/main/groovy/org/supermmx/epug/epub/NavigationItemContainer.groovy
@@ -3,4 +3,10 @@ package org.supermmx.epug.epub
 trait NavigationItemContainer {
     String title
     List<NavigationItem> items = []
+
+    NavigationItemContainer leftShift(NavigationItem item) {
+        items << item
+
+        return this
+    }
 }
