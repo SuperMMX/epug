1888290e45fa8b8ded8503f09f7d49f3591824e1
Xuan MA <SuperMMX@gmail.com>
2021-07-12 22:31:19 +1000

    Add creator and contributor

--- src/main/groovy/org/supermmx/epug/epub/dcmi/DcesTerm.groovy
+++ src/main/groovy/org/supermmx/epug/epub/dcmi/DcesTerm.groovy
@@ -3,7 +3,7 @@ package org.supermmx.epug.epub.dcmi
 class DcesTerm extends DcTerm {
     static final String BASE_URI = 'http://purl.org/dc/elements/1.1/'
 
-    static final DcesTerm contributer = [ baseUri: BASE_URI, name:'contributer'] as DcesTerm
+    static final DcesTerm contributor = [ baseUri: BASE_URI, name:'contributor'] as DcesTerm
     static final DcesTerm identifier = [ baseUri: BASE_URI, name:'identifier'] as DcesTerm
     static final DcesTerm title = [ baseUri: BASE_URI, name:'title'] as DcesTerm
     static final DcesTerm language = [ baseUri: BASE_URI, name:'language'] as DcesTerm
--- src/main/groovy/org/supermmx/epug/epub/dcmi/DcmiTerm.groovy
+++ src/main/groovy/org/supermmx/epug/epub/dcmi/DcmiTerm.groovy
@@ -3,4 +3,5 @@ package org.supermmx.epug.epub.dcmi
 class DcmiTerm extends DcTerm {
     static final String BASE_URI = 'http://purl.org/dc/terms/'
     static final DcmiTerm modified = [baseUri: BASE_URI, name: 'modified'] as DcmiTerm
+    static final DcmiTerm creator = [baseUri: BASE_URI, name: 'creator'] as DcmiTerm
 }
